406bc7974b1440f0f484a9aa14d4f97e5d3873d5
Aidan717 <66300475+Aidan717@users.noreply.github.com>
2022-04-25 22:03:42 +0200

    added laser shots to player

---
+++ FirstProject/Assets/Scripts/LaserShoot.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserShoot : MonoBehaviour
+{
+    [SerializeField]
+    private float _shootSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Update() {
+        LaserMovement();
+        DestroyLaser();
+    }
+
+    private void LaserMovement() {
+        transform.Translate(Vector3.up * Time.deltaTime * _shootSpeed);
+    }
+
+    private void DestroyLaser() {
+        if ( transform.position.y > 8f ) {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- FirstProject/Assets/Scripts/Player.cs
+++ FirstProject/Assets/Scripts/Player.cs
@@ -5,6 +5,12 @@ public class Player : MonoBehaviour
 
     [SerializeField] 
     private float _moveSpeed = 5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _nextFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -13,12 +19,11 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         MovePlayer();
         RestrictPlayer();
+        Shoot();
     }
-
 
     private void MovePlayer() {
 
@@ -40,6 +45,13 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(-11.2f, transform.position.y, 0);
         } else if (transform.position.x <= -11.2f) {
             transform.position = new Vector3(11.2f, transform.position.y, 0);
+        }
+    }
+
+        private void Shoot() {
+        if ( Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFire) {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+            _nextFire = Time.time + _fireRate;
         }
     }
 }
