bf8d0a9f3feb2d0b9bed647491909330cd72b525
Aidan717 <66300475+Aidan717@users.noreply.github.com>
2022-04-30 09:14:27 +0200

    added spawn manager

--- FirstProject/Assets/Scripts/Player.cs
+++ FirstProject/Assets/Scripts/Player.cs
@@ -15,10 +15,17 @@ public class Player : MonoBehaviour
     [SerializeField]
     private int _lives = 3;
 
+    private SpawnManager _spawnManager;
+
     // Start is called before the first frame update
     void Start()
     {
+
         transform.position = new Vector3(0,0,0);
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+         if (_spawnManager == null) {
+            Debug.LogError("SpawnManager is NULL.");
+        }
     }
 
     // Update is called once per frame
@@ -62,6 +69,7 @@ public class Player : MonoBehaviour
         _lives = _lives - arg_damage;
 
         if ( _lives <= 0 ) {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
         
